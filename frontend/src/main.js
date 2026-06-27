8837b19d9a49830909acd043c2073a8b6b51b06f
Simon Österberg <5444093+osterbergsimon@users.noreply.github.com>
2026-06-28 01:07:49 +0200

    feat(design): self-host type-theme fonts via @fontsource (OFL-1.1)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./styles/main.css";
 import "./styles/theme.css";
+import "./styles/fonts.js";
 import { initLogger, logError, logInfo } from "./utils/logger";
 import { useConfigStore } from "./stores/config";
 
